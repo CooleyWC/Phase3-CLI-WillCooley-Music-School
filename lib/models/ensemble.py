579fa31350543625b689d98ae1cae0ec0f6b0b54
Will Cooley <william.cooley@gmail.com>
2023-12-21 18:17:25 -0500

    start project

---
+++ lib/models/ensemble.py
@@ -0,0 +1,10 @@
+from models.__init__ import CURSOR, CONN
+
+
+class Ensemble:
+    
+    def __init__(self, name, director, level):
+        self.id = id
+        self.name = name
+        self.director = director
+    
