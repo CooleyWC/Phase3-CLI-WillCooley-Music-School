76af99b29f6ed5be4f30c86ca62957ae321e0d40
Will Cooley <william.cooley@gmail.coom>
2023-12-23 22:10:38 -0500

    updated debug file

--- lib/debug.py
+++ lib/debug.py
@@ -6,12 +6,20 @@ from models.ensemble import Ensemble
 from models.musician import Musician
 import ipdb
 
-# will = Musician('will', 'drums', 45, 33, 'yes')
-# print(will)
+def reset_database():
+    Musician.drop_table()
+    Ensemble.drop_table()
+    Ensemble.create_table()
+    Musician.create_table()
 
-jazz_ensemble_1 = Ensemble('jazz ensemble 1', 'basie', 'advanced')
-print(jazz_ensemble_1)
+    # seed data
+    jazz_ensemble_1 = Ensemble.create('Jazz Ensemble 1', 'basie', 'advanced')
+    jazz_combo_1 = Ensemble.create('Jazz Combo 1', 'gunther', 'advanced')
+    Musician.create('Philly Joe Jones', 'drums', 45, 99, 'no', 1)
+    Musician.create('McCoy Tyner', 'piano', 25, 87, 'yes', 2)
+    Musician.create('Miles Davis', 'trumpet', 26, 78, 'yes', 2)
+    Musician.create('Ron Carter', 'bass', 42, 89, 'yes', 1)
+    Musician.create('Charlie Parker', 'alto saxophone', 76, 98, 'no', 1)
 
-
-
+reset_database()
 ipdb.set_trace()
