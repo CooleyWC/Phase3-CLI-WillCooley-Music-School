c9c54c05dc9134f9029f476a20806f16ec7bddca
Will Cooley <william.cooley@gmail.com>
2024-01-14 12:56:08 -0500

    updated README

--- lib/seed.py
+++ lib/seed.py
@@ -21,9 +21,11 @@ def seed_database():
     Musician.create('Philly Joe Jones', 'drums', 45, 99, 'no', jazz_ensemble_1.id)
     Musician.create('McCoy Tyner', 'piano', 25, 87, 'yes', jazz_combo_1.id)
     Musician.create('Chet Baker', 'trumpet', 26, 78, 'yes', jazz_combo_1.id)
+    Musician.create('Norah Jones', 'piano', 40, 91, 'yes', jazz_ensemble_1.id)
     Musician.create('Ron Carter', 'bass', 42, 89, 'yes', jazz_ensemble_1.id)
     Musician.create('Charlie Parker', 'alto saxophone', 76, 98, 'no', jazz_ensemble_1.id)
     Musician.create('Tom Morello', 'guitar', 56, 75, 'yes', rock_band_1.id)
+    Musician.create('Taylor Swift', 'guitar', 31, 89, 'no', jazz_combo_1.id)
     Musician.create('Jim Ross', 'drums', 34, 54, 'no', chamber_ensemble_2.id)
     Musician.create('Chris Cornell', 'voice', 34, 53, 'no', rock_band_1.id)
     Musician.create('Chuck Mangione', 'trumpet', 45, 78, 'yes', jazz_ensemble_1.id)
@@ -31,6 +33,7 @@ def seed_database():
     Musician.create('Chris Lamb', 'drums', 34, 78, 'yes', chamber_ensemble_2.id)
     Musician.create('Jack Johnson', 'trombone', 46, 87, 'yes', jazz_orchestra_3.id)
     Musician.create('Chris Marton', 'trumpet', 44, 89, 'no', symphonic_winds.id)
+    Musician.create('Patsy Dash', 'drums', 78, 97, 'no', symphonic_winds.id)
 
 
 seed_database()
